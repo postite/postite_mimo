5c380b039c1dca7b778fbf7c0c38ea82b5193924
postite <blog@postite.com>
2019-04-30 13:04:06 +0200

    remove extra param

--- tests/SimpleTest.hx
+++ tests/SimpleTest.hx
@@ -1,12 +1,5 @@
-import haxe.io.Mime;
 import haxe.io.Path;
-import sys.io.FileInput;
 import sys.FileSystem;
-import haxe.io.ArrayBufferView;
-import haxe.Resource;
-import sys.io.File;
-import mimo.MimoType;
-import mimo.Mimo;
 import utest.Assert;
 class SimpleTest extends utest.Test{
     var images:Array<String>;
